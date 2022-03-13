ecf52a20770341596c174222478ad55d6ab43982
Josh Beard <josh@joshbeard.me>
2022-03-12 22:14:45 -0700

    Remove obsoleted 'dkim' attribute

--- site/main.tf
+++ site/main.tf
@@ -10,7 +10,6 @@ module "jbeardllc_com_aws" {
 module "jbeardllc_com_migadu" {
     source = "github.com/joshbeard/tf-migadu-route53.git"
 
-    dkim          = "v=DKIM1; k=rsa; s=email; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCwLut0Bap/mh3HE2rDPchHVrEcaKAfE1X5/k+7fBiIdRgk8GFoMiG3WpbaWygLS0Hw03ZjdeiIPQW/bfB7tz88NIwhIlq4VI2w+oBjF9pciLgzu0gFRGEC1KdRjn7M3YD/KTURS9kAAkgLKFWXqrUsuhuM00mZQn72MA/N9DSPfwIDAQAB"
     domain        = "jbeardllc.com"
     zone_id       = module.jbeardllc_com_aws.zone_id
     migadu_verify = "kvjc7pvy"
