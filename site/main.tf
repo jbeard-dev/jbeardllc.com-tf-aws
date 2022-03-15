2f982549ae3c7b04a4cc8e7337b7b9841a5d97cb
Josh Beard <josh@joshbeard.me>
2022-03-15 17:25:30 -0600

    Linting; Add 'git_repo' tag; Update module reference for zone_id

--- site/main.tf
+++ site/main.tf
@@ -1,16 +1,17 @@
 # Deployment of AWS resources for jbeardllc.com
 module "jbeardllc_com_aws" {
-    source = "github.com/joshbeard/tf-aws-site.git"
+  source = "github.com/joshbeard/tf-aws-site.git"
 
-    domain      = "jbeardllc.com"
-    bucket_name = "s3-website-jbeardllc-com"
-    iam_name    = "s3-deployer-jbeardllc-com"
+  domain      = "jbeardllc.com"
+  bucket_name = "s3-website-jbeardllc-com"
+  iam_name    = "s3-deployer-jbeardllc-com"
+  tags        = { "git_repo" : "https://github.com/joshbeard/jbeardllc.com-tf-aws" }
 }
 
 module "jbeardllc_com_migadu" {
-    source = "github.com/joshbeard/tf-migadu-route53.git"
+  source = "github.com/joshbeard/tf-migadu-route53.git"
 
-    domain        = "jbeardllc.com"
-    zone_id       = module.jbeardllc_com_aws.zone_id
-    migadu_verify = "kvjc7pvy"
+  domain        = "jbeardllc.com"
+  zone_id       = module.jbeardllc_com_aws.route53_zone_id
+  migadu_verify = "kvjc7pvy"
 }
